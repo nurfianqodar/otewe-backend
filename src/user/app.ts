88340686952ab8ed5582840ad947a48bd692eb37
nurfianqodar <77nurfianqodar@gmail.com>
2024-06-18 09:35:25 +0700

    formatting

--- src/user/app.ts
+++ src/user/app.ts
@@ -15,11 +15,10 @@ const userApp = new Hono();
 userApp.get("/", ...getUserListHandler);
 userApp.post("/", ...createUserHandler);
 userApp.get("/profile/:id", ...getByIdHandler);
-
 userApp.get("/me", ...getMeHandler);
 userApp.patch("/me", ...updateMeHandler);
 userApp.delete("/me", ...deleteMeHandler);
-userApp.patch("/me/update-password", ...updatePasswordMeHandler);
 userApp.patch("/me/update-unique", ...updateUniqueMeHandler);
+userApp.patch("/me/update-password", ...updatePasswordMeHandler);
 
 export default userApp;
